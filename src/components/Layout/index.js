9927661334520204797e12f6f7670650b11582db
nathaliacrz <nathalias161@gmail.com>
2022-08-25 14:16:46 -0300

    personalizando side bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,30 @@
+import { Outlet } from 'react-router-dom';
+import Home from '../Home';
+import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
-    return <>Hello</>
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>
+                    &lt;body&gt;
+                </span>
+
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+
+            &lt;body&gt;
+            <br/>
+                <span className='bottom-tag-html'>
+            <Home/>
+                </span>
+            </span>
+            </div>
+        </div>
+    )
 }
 
 export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import './index.scss'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={Logo} alt='logo' />
+        </Link>
+
+        <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+
+            <NavLink exact='true' activeclassname='active' className='about-link' to='/'>
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+
+            <NavLink exact='true' activeclassname='active' className='contact-link' to='/'>
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a href='https://www.linkedin.com/in/nathalia-cruz-6b1561238/' target='_blank' rel='noreferrer' className='anchor-icon'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+
+                <a href='https://github.com/nathaliacrz' target='_blank' rel='noreferrer' className='anchor-icon'>
+                    <FontAwesomeIcon icon={faGithub}/>
+                </a>
+
+                <a href='https://www.instagram.com/tthanalia/' target='_blank' rel='noreferrer' className='anchor-icon'>
+                    <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+)
+
+export default Sidebar;
