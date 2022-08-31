922eff66d6b5d1f624a1862c25693e9764567bf9
nathaliacrz <nathalias161@gmail.com>
2022-08-31 16:54:15 -0300

    formulario pagina  de contato

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,84 @@
+import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters/';
 import './index.scss'
 
 const Contact = () => {
 
-    return (
-    <>
-        
-        <div className='container contact-page'>
+    const [letterClass, setLetterClass] = useState('text-animate');
 
-        </div>
-        
-        <Loader type='ball-zig-zag'/>
-    </>
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 6000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 't', 'o']}
+                            idx={15} />
+                    </h1>
+                    <p>
+                        Me identifico mais com a área de back-end, porém consigo atuar bem como full-stack. De qualquer maneira, se tiver alguma pergunta ou proposta, pode me mandar por e-mail utilizando o formulário abaixo!!
+                    </p>
+                    <div className='contact-form'>
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input
+                                        type='text'
+                                        name='name'
+                                        placeholder='Nome'
+                                        required
+                                    />
+                                </li>
+                                <li className='half'>
+                                    <input
+                                        type='email'
+                                        name='email'
+                                        placeholder='E-mail'
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder='Conteúdo'
+                                        type='text'
+                                        name='subject'
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder='Mensagem'
+                                        name='message'
+                                        required
+                                        cols="30"
+                                        rows="10"
+                                    >
+                                    </textarea>
+                                </li>
+                                <li>
+                                    <input
+                                        type="submit"
+                                        className='flat-button'
+                                        value='Enviar'
+                                    />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <Loader type='ball-zig-zag' />
+        </>
 
     )
 }
